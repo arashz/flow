ef053f1e87f0b9a79b2942b08ed28aafe7b09460
Arash Zolfaghari <arashz@gmail.com>
2020-11-22 19:22:58 -0800

    testing to changing background color when button pressed

--- src/components/MindNode.js
+++ src/components/MindNode.js
@@ -1,54 +1,310 @@
-import React, { Fragment, useState } from 'react';
-import ReactFlow, {addEdge, Background, Controls, MiniMap } from 'react-flow-renderer';
+import React, { useState, Fragment, useEffect } from 'react';
+
+import ReactFlow, {
+  addEdge,
+  Background,
+  Controls,
+  MiniMap,
+  removeElements,
+  ReactFlowProvider,
+} from 'react-flow-renderer';
+
+/*const initialElements = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'Mind Node' },
+    position: { x: 0, y: 0 },
+  },
+];*/
+
 const initialElements = [
-  { id: '1', type: 'input', data: { label: 'Node' }, position: { x: 0, y: 0 } },
+  {
+    id: '1',
+    type: 'input',
+    data: {
+      label: (
+        <>
+          Welcome to <strong>React Flow!</strong>
+        </>
+      ),
+    },
+    style: {
+      background: '#D6D5E6',
+      color: '#333',
+      border: '1px solid #222138',
+      width: 180,
+    },
+    position: { x: 250, y: 0 },
+  },
+  {
+    id: '2',
+    data: {
+      label: (
+        <>
+          This is a <strong>default node</strong>
+        </>
+      ),
+    },
+    style: {
+      background: '#D6D5E6',
+      color: '#333',
+      border: '1px solid #222138',
+      width: 180,
+    },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '3',
+    data: {
+      label: (
+        <>
+          This one has a <strong>custom style</strong>
+        </>
+      ),
+    },
+    position: { x: 400, y: 100 },
+    style: {
+      background: '#D6D5E6',
+      color: '#333',
+      border: '1px solid #222138',
+      width: 180,
+    },
+  },
+  {
+    id: '4',
+    position: { x: 250, y: 200 },
+    data: {
+      label: 'Another default node',
+    },
+    style: {
+      background: '#D6D5E6',
+      color: '#333',
+      border: '1px solid #222138',
+      width: 180,
+    },
+  },
+  {
+    id: '5',
+    data: {
+      label: 'Node id: 5',
+    },
+    style: {
+      background: '#D6D5E6',
+      color: '#333',
+      border: '1px solid #222138',
+      width: 180,
+    },
+    position: { x: 250, y: 325 },
+  },
+  {
+    id: '6',
+    type: 'output',
+    data: {
+      label: (
+        <>
+          An <strong>output node</strong>
+        </>
+      ),
+    },
+    style: {
+      background: '#D6D5E6',
+      color: '#333',
+      border: '1px solid #222138',
+      width: 180,
+    },
+    position: { x: 100, y: 480 },
+  },
+  {
+    id: '7',
+    type: 'output',
+    data: { label: 'Another output node' },
+    style: {
+      background: '#D6D5E6',
+      color: '#333',
+      border: '1px solid #222138',
+      width: 180,
+    },
+    position: { x: 400, y: 450 },
+  },
+  { id: 'e1-2', source: '1', target: '2', label: 'this is an edge label' },
+  { id: 'e1-3', source: '1', target: '3' },
+  {
+    id: 'e3-4',
+    source: '3',
+    target: '4',
+    animated: true,
+    label: 'animated edge',
+  },
+  {
+    id: 'e4-5',
+    source: '4',
+    target: '5',
+    arrowHeadType: 'arrowclosed',
+    label: 'edge with arrow head',
+  },
+  {
+    id: 'e5-6',
+    source: '5',
+    target: '6',
+    type: 'smoothstep',
+    label: 'smooth step edge',
+  },
+  {
+    id: 'e5-7',
+    source: '5',
+    target: '7',
+    type: 'step',
+    style: { stroke: '#f6ab6c' },
+    label: 'a step edge',
+    animated: true,
+    labelStyle: { fill: '#f6ab6c', fontWeight: 700 },
+  },
 ];
 
-const onLoad = (reactFlowInstance) =>{
-    reactFlowInstance.fitView()
-}
 const MindNode = () => {
+  const [elements, setElements] = useState(initialElements);
+  const [name, setName] = useState('');
 
-    const [elements, setElements] = useState(initialElements);
-    const [name, setName] = useState("")
+  const onChange = elements => {
+    setElements(elements);
+    console.log('onChange', elements);
+  };
 
-    const addNode = () =>{
-        setElements(e => e.concat({
-            id: (e.length+1).toString(),
-            data: {label: `${name}`},
-            position: {x: Math.random()* window.innerWidth, y: Math.random()*window.innerHeight }
-        }))
-    }
+  const onLoad = reactFlowInstance => {
+    reactFlowInstance.fitView();
+  };
 
-    const onConnect = (params)=> setElements(e=> addEdge(params,e))
+  const onElementsRemove = elementsToRemove => {
+    console.log('INSIDE onElementsRemove');
+    onChange(removeElements(elementsToRemove, elements));
+  };
+
+  const onConnect = params => {
+    console.log('INSIDE onConnect');
+    onChange(addEdge(params, elements));
+  };
+
+  //const onConnect = params => setElements(e => addEdge(params, e));
+
+  const onSelectionDragStop = (params, nodes) => {
+    console.log('INSIDE onSelectionDragStop');
+    onChange([...removeElements(nodes, elements), ...nodes]);
+  };
+
+  const onNodeDragStop = (params, node) => {
+    console.log('INSIDE onNodeDragStop');
+    onChange(
+      elements.map(element => (element.id === node.id ? node : element)),
+    );
+  };
+
+  useEffect(() => {
+    console.log('USE EFFECT CALLED');
+    // Update the document title using the browser API
+    //document.title = `You clicked ${count} times`;
+  }, []);
+
+  const addNode = () => {
+    setElements(e =>
+      e.concat({
+        id: (e.length + 1).toString(),
+        data: { label: `${name}` },
+        position: {
+          x: Math.random() * window.innerWidth,
+          y: Math.random() * window.innerHeight,
+        },
+      }),
+    );
+  };
+
+  const onElementClick = (event, element) => {
+    console.log('INSIDE onElementClick', element);
+  };
+
+  const nice = () => {
+    //let elements2 = elements;
+    /*elements.map(x => {
+      if (x && x.style && x.style.background) {
+        x.style.background =
+          '#' + Math.floor(Math.random() * 16777215).toString(16);
+      }
+      return x;
+    });*/
+    //console.log('NEW elements2', elements2);
+
+    const timer = setInterval(() => {
+      onChange(
+        elements.map(element => {
+          if (element && element.style && element.style.background) {
+            element.style.background =
+              '#' + Math.floor(Math.random() * 16777215).toString(16);
+          }
+          return element;
+        }),
+      );
+    }, 1000);
+    onChange(
+      elements.map(element => {
+        if (element && element.style && element.style.background) {
+          element.style.background =
+            '#' + Math.floor(Math.random() * 16777215).toString(16);
+        }
+        return element;
+      }),
+    );
+  };
+
   return (
     <Fragment>
-      <ReactFlow
-        elements={elements}
-        onLoad={onLoad}
-        style={{ width: '100%', height: '90vh' }}
-        onConnect={onConnect}
-        connectionLineStyle = {{stroke: "#ddd", strokeWidth:2}}
-        connectionLineType = "bezier"
-        snapToGrid = {true}
-        snapGrid={[16,16]}
-      >
-        <Background
-            color="#888"
-            gap={16}
-        />
-        <MiniMap
-          nodeColor={n => {
-            if (n.type === 'input') return 'blue';
-            return '#FFCC00';
+      <ReactFlowProvider>
+        <ReactFlow
+          elements={elements}
+          onLoad={onLoad}
+          style={{ width: '100%', height: '90vh' }}
+          onConnect={onConnect}
+          connectionLineStyle={{ stroke: '#ddd', strokeWidth: 20 }}
+          connectionLineType="SmoothStep"
+          snapToGrid={true}
+          snapGrid={[16, 16]}
+          onPaneClick={() => {
+            console.log('INSIDE onPaneClick');
           }}
-        />
-      </ReactFlow>
+          onPaneScroll={() => {
+            console.log('INSIDE onPaneScroll');
+          }}
+          onElementsRemove={onElementsRemove}
+          onElementClick={onElementClick}
+          onSelectionDragStop={onSelectionDragStop}
+          onNodeDragStop={onNodeDragStop}
+        >
+          <Background color="#888" gap={16} />
+          <MiniMap
+            nodeColor={n => {
+              if (n.type === 'input') return 'blue';
 
-      <div>
-        <input type="text" name="title" onChange={e=> setName(e.target.value)} />
-        <button type="button" onClick={addNode}>Add Node</button>
-      </div>
+              return '#FFCC00';
+            }}
+          />
+          <Controls />
+        </ReactFlow>
+
+        <div>
+          <input
+            type="text"
+            onChange={e => setName(e.target.value)}
+            name="title"
+          />
+          <button type="button" onClick={addNode}>
+            Add Node
+          </button>
+          <br />
+          <br />
+          <button type="button" onClick={nice}>
+            Press to change background color every second
+          </button>
+        </div>
+      </ReactFlowProvider>
     </Fragment>
   );
 };
